f6a17969ef9a7b3530a505d673296fe11de706fc
Endymion Kasanardjo <zetafish@gmail.com>
2020-12-06 10:02:20 +0100

    day 6 - part 2

--- src/aoc/d6.clj
+++ src/aoc/d6.clj
@@ -1,12 +1,20 @@
 (ns aoc.d6
   (:require [clojure.string :as str]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [clojure.set :as set]))
 
-(def input (str/split-lines (slurp (io/resource "d6.txt"))))
+(def input
+  (->>
+   (str/split-lines (slurp (io/resource "d6.txt")))
+   (partition-by str/blank?)
+   (remove #{[""]})))
 
 ;; part 1
 (->> input
-     (partition-by str/blank?)
-     (remove #{[""]})
      (map (comp count set (partial apply concat)))
+     (reduce +))
+
+;; part 2
+(->> input
+     (map (comp count (partial apply set/intersection) (partial map set)))
      (reduce +))
