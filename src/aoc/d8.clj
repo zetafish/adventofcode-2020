139279c25abbfa489859d03891e135b6d4cc44a1
Endymion Kasanardjo <zetafish@gmail.com>
2020-12-10 18:29:26 +0100

    day 8 - part 2

--- src/aoc/d8.clj
+++ src/aoc/d8.clj
@@ -2,43 +2,59 @@
   (:require [clojure.string :as str]
             [clojure.java.io :as io]))
 
-(def input (str/split-lines (slurp (io/resource "d8.txt"))))
-
-(def example ["nop +0"
-              "acc +1"
-              "jmp +4"
-              "acc +3"
-              "jmp -3"
-              "acc -99"
-              "acc +1"
-              "jmp -4"
-              "acc +6"])
-
 (defn parse [s]
   (let [[cmd n] (str/split s #" ")]
     [(keyword cmd) (Integer/parseInt n)]))
 
-(map parse input)
+(def input (mapv parse (str/split-lines (slurp (io/resource "d8.txt")))))
 
-(map parse example)
+(def example (mapv parse ["nop +0"
+                          "acc +1"
+                          "jmp +4"
+                          "acc +3"
+                          "jmp -3"
+                          "acc -99"
+                          "acc +1"
+                          "jmp -4"
+                          "acc +6"]))
 
-(defn step [{:keys [acc slots ip seen halted] :as state}]
+(defn step [{:keys [acc slots ip seen] :as state}]
   (let [[cmd n] (get slots ip)]
-    (if (seen ip)
-      (assoc state :halted true)
-      (->
-       (case cmd
-         :nop (update state :ip inc)
-         :jmp (update state :ip + n)
-         :acc (-> state
-                  (update :ip inc)
-                  (update :acc + n)))
-       (update :seen conj ip)))))
+    (cond
+      (:exit-code state) (assoc state :halted true)
+      (seen ip) (assoc state :exit-code :loop-detected)
+      (= ip (count slots)) (assoc state :exit-code :completed)
+      :else (->
+             (case cmd
+               :nop (update state :ip inc)
+               :jmp (update state :ip + n)
+               :acc (-> state (update :ip inc) (update :acc + n)))
+             (update :seen conj ip)))))
 
 (defn run
   [slots]
-  (iterate step {:acc 0 :slots slots :ip 0 :seen #{}}))
+  (->> {:acc 0 :slots slots :ip 0 :seen #{}}
+       (iterate step)
+       (take-while (complement :halted))
+       last
+       ((juxt :exit-code :acc))))
 
 ;; part 1
-(->> (mapv parse example) run (take-while (complement :halted)) last :acc)
-(->> (mapv parse input) run (take-while (complement :halted)) last :acc)
+(run example)
+(run input)
+
+(defn swap [slots i]
+  (case (first (slots i))
+    :jmp (assoc-in slots [i 0] :nop)
+    :nop (assoc-in slots [i 0] :jmp)
+    slots))
+
+(defn fix-bug
+  [slots]
+  (->> (range (count slots))
+       (map #(swap slots %))
+       (map run)
+       (filter (comp #{:completed} first))))
+
+(fix-bug example)
+(fix-bug input)
