f0142a5e31a0cd5759fa98ace4018bf3d998becf
Endymion Kasanardjo <zetafish@gmail.com>
2020-12-11 00:35:56 +0100

    day 10 - part 2

--- src/aoc/d10.clj
+++ src/aoc/d10.clj
@@ -8,12 +8,62 @@
 (def example1 [16 10 15 5 1 11 7 19 6 12 4])
 (def example2 [28 33 18 42 31 14 46 20 48 47 24 23 49 45 19 38 39 11 1 32 25 35 8 17 7 9 4 2 34 10 3])
 
+(defn build-chain [coll]
+  (let [coll (sort coll)]
+    (vec (concat [0] coll [(+ 3 (last coll))]))))
+
 (defn jolt-distribution
   [coll]
-  (let [coll (conj (sort coll) 0)]
-    (frequencies (conj (map #(- %2 %1) coll (drop 1 coll)) 3))))
+  (frequencies
+   (map #(- %2 %1) coll (drop 1 coll))))
 
 ;; part 1
-(jolt-distribution example1)
-(jolt-distribution example2)
-(reduce * (vals (jolt-distribution input)))
+(jolt-distribution (build-chain example1))
+(jolt-distribution (build-chain example2))
+(reduce * (vals (jolt-distribution (build-chain input))))
+
+(defn can-remove-at?
+  [n coll]
+  (<= (- (nth coll (inc n)) (nth coll (dec n))) 3))
+
+(defn count-ways
+  [coll]
+  (loop [seen #{coll} todo [coll]]
+    (if (empty? todo)
+      (count seen)
+      (let [coll (vec (first todo))
+            candidates (->> (drop 1 (range (dec (count coll))))
+                            (filter #(can-remove-at? % coll))
+                            (map #(assoc coll % nil))
+                            (map #(remove nil? %))
+                            (remove seen))]
+        (recur (into seen candidates)
+               (into (rest todo) candidates))))))
+
+(defn find-clusters
+  [coll]
+  (->> coll
+       (map-indexed (fn [i x] [(and (< 0 i (dec (count coll))) (can-remove-at? i coll)) x]))
+       (reduce (fn [{:keys [result segment inside prev] :as acc} [can-remove? current]]
+                 (let [acc (assoc acc
+                                  :inside can-remove?
+                                  :prev current)]
+                   (cond
+                     ;; we are still inside a segment
+                     (and inside can-remove?) (update acc :segment conj current)
+
+                     ;; segment ends
+                     inside (update acc :result conj (conj segment current))
+
+                     ;; segment starts
+                     can-remove? (assoc acc :segment [prev current])
+
+                     ;; we are still outside
+                     :else acc)))
+               {})
+       :result))
+
+;; part 2
+(->> example1 build-chain find-clusters (map count-ways) (reduce *))
+(->> example2 build-chain find-clusters (map count-ways) (reduce *))
+(->> input  build-chain find-clusters (map count-ways) (reduce *))
