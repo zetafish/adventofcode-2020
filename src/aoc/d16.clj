d36ba75e3e49961a3c86448be94c70f1c43175e1
Endymion Kasanardjo <zetafish@gmail.com>
2020-12-20 16:24:43 +0100

    day 16 - part 2

--- src/aoc/d16.clj
+++ src/aoc/d16.clj
@@ -15,15 +15,50 @@
 (defn parse-ticket [s]
   (map parse-int (str/split s #",")))
 
-(def input (let [s (str/split-lines (slurp (io/resource "d16.txt")))
+(def data (let [s (str/split-lines (slurp (io/resource "d16.txt")))
                  [fields my nearby] (remove #(= [""] %) (partition-by str/blank? s))]
              {:fields (map parse-field fields)
               :my (parse-ticket (second my))
               :nearby (map parse-ticket (drop 1 nearby))}))
 
-(defn field-checker
-  [fields]
-  (fn [n]
-    (some  #((:valid? %) n) fields)))
+(def fields (:fields data))
+(def n-fields (count (:fields data)))
+(def my (:my data))
+(def nearby (:nearby data))
 
-(reduce + (remove (field-checker (:fields input)) (apply concat (:nearby input))))
+(defn field-valid?
+  [n]
+  (some  #((:valid? %) n) (:fields data)))
+
+;; part 1
+(reduce + (remove field-valid? (apply concat (:nearby data))))
+
+(def valid-tickets (filter (partial every? field-valid?) (:nearby data)))
+
+(defn candidate-positions
+  [{:keys [valid?]}]
+  (->> (range n-fields)
+       (map (fn [i] [i (every? valid? (map #(nth % i) valid-tickets))]))
+       (filter second)
+       (map first)))
+
+(def assignments
+  (loop [m {}
+         seen #{}
+         candidates (->> fields
+                         (map candidate-positions)
+                         (zipmap fields)
+                         (sort-by (comp count second)))]
+    (if (empty? candidates)
+      m
+      (let [[f c] (first candidates)
+            c (remove seen c)]
+        (assert (= 1 (count c)))
+        (recur (assoc m f (first c)) (conj seen (first c)) (rest candidates))))))
+
+(->> fields
+     (filter #(str/starts-with? (:name %) "departure"))
+     (map assignments)
+     (map #(nth my %))
+     (reduce *)
+     (println))
