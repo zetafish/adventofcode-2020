d905589b37de578f182f3b87d711efe29c2b9501
Endymion Kasanardjo <zetafish@gmail.com>
2020-12-20 21:42:05 +0100

    day 18 - part 2

--- src/aoc/d18.clj
+++ src/aoc/d18.clj
@@ -3,36 +3,62 @@
             [instaparse.core :as insta]
             [clojure.java.io :as io]))
 
-(def g "
-  <E>=num|mul|add|par
+(def grammar "
+  E=num|mul|add|par
   num=#'\\d+'
   mul=E <' * '> (num|par)
   add=E <' + '> (num|par)
   par=<'('> E <')'>
-")
+  ")
 
-(defn parse [s]
-  (first
-   (insta/transform
-    {:num #(Integer/parseInt %)}
-    (insta/parse (insta/parser g) s))))
+(defn parens
+  "See: https://en.wikipedia.org/wiki/Operator-precedence_parser
 
-(def input (map parse (str/split-lines (slurp (io/resource "d18.txt")))))
+  Trick to correctly have the operator precendence with LR parser:
+  - replace + with ) + (
+  - replace * with )) * ((
+  - add (( at the beginning and after each left-paren in the original
+  - add )) at the beginning and before each right-paren in the original
+  "
+  [s]
+  (str "(("
+       (-> s
+           (str/replace #" " "")
+           (str/replace #"\(" "(((")
+           (str/replace #"\)" ")))")
+           (str/replace #"\+" ") + (")
+           (str/replace #"\*" ")) * (("))
+       "))"))
 
-(first input)
+(defn parse
+  [s]
+  (insta/transform
+   {:E identity
+    :num #(Integer/parseInt %)
+    :add (fn [& args] (conj args '+))
+    :mul (fn [& args] (conj args '*))
+    :par identity}
+   (insta/parse (insta/parser grammar) s)))
 
-(defn eval-expr [expr]
-  (cond
-    (number? expr) expr
-    (= :par (first expr)) (eval-expr (second expr))
-    (= :mul (first expr)) (* (eval-expr (nth expr 1))
-                             (eval-expr (nth expr 2)))
-    (= :add (first expr)) (+ (eval-expr (nth expr 1))
-                             (eval-expr (nth expr 2)))))
+(def input (str/split-lines (slurp (io/resource "d18.txt"))))
 
-(eval-expr (parse "1 + 2 * 3 + 4 * 5 + 6"))
-(eval-expr (parse "1 + (2 * 3) + (4 * (5 + 6))"))
-(eval-expr (parse "2 * 3 + (4 * 5)"))
-(eval-expr (parse "5 + (8 * 3 + 9 + 3 * 4 * 3)"))
-(eval-expr (parse "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"))
-(eval-expr (parse "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"))
+(eval (parse "1 + 2 * 3 + 4 * 5 + 6"))
+(eval (parse "1 + (2 * 3) + (4 * (5 + 6))"))
+(eval (parse "2 * 3 + (4 * 5)"))
+(eval (parse "5 + (8 * 3 + 9 + 3 * 4 * 3)"))
+(eval (parse "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"))
+(eval (parse "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"))
+(->> input
+     (map (comp eval parse))
+     (reduce +))
+
+
+(eval (parse (parens "1 + 2 * 3 + 4 * 5 + 6")))
+(eval (parse (parens "1 + (2 * 3) + (4 * (5 + 6))")))
+(eval (parse (parens "2 * 3 + (4 * 5)")))
+(eval (parse (parens "5 + (8 * 3 + 9 + 3 * 4 * 3)")))
+(eval (parse (parens "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")))
+(eval (parse (parens "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")))
+(->> input
+     (map (comp eval parse parens))
+     (reduce +))
