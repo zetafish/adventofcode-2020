71ae7cac8cf1874a559f9c66e06712a6ad1ca9fa
Endymion Kasanardjo <zetafish@gmail.com>
2020-12-22 10:14:28 +0100

    day 22 - part 2

--- src/aoc/d22.clj
+++ src/aoc/d22.clj
@@ -4,7 +4,7 @@
 
 (defn parse-int [s] (Integer/parseInt s))
 
-(def decks (->> (str/split-lines (slurp (io/resource "d22.txt")))
+(def input (->> (str/split-lines (slurp (io/resource "d22.txt")))
                 (partition-by str/blank?)
                 (remove #{[""]})
                 (map #(drop 1 %))
@@ -13,7 +13,8 @@
 
 (def example [[9 2 6 3 1] [5 8 4 7 10]])
 
-(defn play [d1 d2]
+(defn combat
+  [d1 d2]
   (cond
     (empty? d1) [:player-2 d2]
     (empty? d2) [:player-1 d1]
@@ -23,4 +24,38 @@
               (< c1 c2) (recur (rest d1) (concat (rest d2) [c2 c1]))
               (< c2 c1) (recur (concat (rest d1) [c1 c2]) (rest d2))))))
 
-(play (first example) (second example))
+(defn score [deck]
+  (reduce + (map-indexed (fn [i c] (* c (inc i))) (reverse deck))))
+
+;; part 1
+(score (second (combat (first example) (second example))))
+(score (second (combat (first input) (second input))))
+
+(defn recursive-combat
+  ([d1 d2] (recursive-combat #{} d1 d2))
+  ([seen d1 d2]
+   (cond
+     (contains? seen [d1 d2]) [:player-1 d1 d2]
+     (empty? d1) [:player-2 d2]
+     (empty? d2) [:player-1 d1]
+     :else (let [c1 (first d1)
+                 c2 (first d2)
+                 winner (cond
+                          (and (< c1 (count d1)) (< c2 (count d2)))
+                          (first (recursive-combat #{}
+                                                   (take c1 (drop 1 d1))
+                                                   (take c2 (drop 1 d2))))
+                          (< c1 c2) :player-2
+                          (< c2 c1) :player-1)]
+             (case winner
+               :player-2 (recur (conj seen [d1 d2])
+                                (rest d1)
+                                (concat (rest d2) [c2 c1]))
+               :player-1 (recur (conj seen [d1 d2])
+                                (concat (rest d1) [c1 c2])
+                                (rest d2)))))))
+
+;; part 2
+(recursive-combat [9, 2, 6, 3, 1] [5, 8, 4, 7, 10])
+(score (second (recursive-combat (first example) (second example))))
+(score (second (recursive-combat (first input) (second input))))
